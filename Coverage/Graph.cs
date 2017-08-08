b012344f122d3afb9742422aa0ee63a34e064ef4
Andrea Nistico <andrea.nistic@gmail.com>
2017-08-08 14:27:52 +0200

    initial commit: base graph representation

---
+++ Coverage/Graph.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Coverage
+{
+    namespace Navigation
+    {
+
+        //Basic class for link representation between nodes
+        public class Link
+        {
+
+            Node adj;
+            double weight;
+            public Link(Node adj, double weight)
+            {
+                this.adj = adj;
+                this.weight = weight;
+                Console.WriteLine("Link  created");
+            }
+            public Node getAdj()
+            {
+                return adj;
+            }
+            public double getWeight()
+            {
+                return weight;
+            }
+            public void setWeight(double w)
+            {
+                weight = w;
+            }
+        }//CLASS Link
+
+        //Simple graph node
+        public class Node
+        {
+
+            int id;
+            public bool isActive { get; set; }
+            public List<Link> links;
+
+            public Node(int id)
+            {
+                this.id = id;
+                isActive = true;
+                links = new List<Link>();
+                Console.WriteLine("Node" + id + " created");
+            }
+
+            public int getId()
+            {
+                return id;
+            }
+            public void setId(int idValue)
+            {
+                id = idValue;
+            }
+            public void addLink(Node adj, double weight)
+            {
+                links.Add(new Link(adj, weight));
+            }
+            public void removeLink(int linkId)
+            {
+
+                links.Remove(links[linkId]);
+
+            }
+        }//CLASS Node
+
+        //Simple graph
+        public class Graph
+        {
+
+            int num_vert = 0;
+            //Weight representation used for convenience
+            double[,] w_matrix;
+            public List<Node> nodes = new List<Node>();
+
+            public Graph()
+            {
+                Console.WriteLine("Graph created!");
+            }
+
+            public void computeWeights()
+            {
+
+                //Nodes are sorted by ID number
+                nodes.Sort((Node x, Node y) => x.getId().CompareTo(y.getId()));
+
+                foreach (var item in nodes)
+                {
+                    Console.WriteLine(item.getId());
+                }
+
+                //Get number of total nodes
+                num_vert = nodes.Count;
+
+                //Only if we added every node to the list
+                if (num_vert > 0)
+                {
+                    w_matrix = new double[num_vert, num_vert];
+
+                    //Fill weighted matrix
+                    for (int i = 0; i < num_vert; i++)
+                    {
+                        for (int j = 0; j < num_vert; j++)
+                        {
+                            //Set diag to zero
+                            if (i == j)
+                                w_matrix[i, j] = -1;
+                            else
+                            {
+                                //Unreachable by default
+                                w_matrix[i, j] = -1;
+
+                                //Check possible links
+                                if (nodes[i].links.Count > 0)
+                                {
+                                    foreach (var l in nodes[i].links)
+                                    {
+
+                                        if (l.getAdj().getId() == nodes[j].getId())
+                                        {
+
+                                            Console.WriteLine("Node " + nodes[i].getId() + " is connected to node: " + nodes[j].getId() + " with a weight of: " + l.getWeight());
+                                            w_matrix[i, j] = l.getWeight();
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR, node list is empty");
+                    return;
+                }
+            }
+
+            public void printL()
+            {
+
+                for (int i = 0; i < num_vert; i++)
+                {
+                    for (int j = 0; j < num_vert; j++)
+                    {
+                        Console.Write(w_matrix[i, j] + " ");
+                    }
+                    Console.Write("\n");
+                }   
+            }
+        }//CLASS Graph
+    }//NAMESPACE Navigation
+}//NAMESPACE Coverage
---
+++ Coverage/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+
+namespace Coverage
+{
+    using  Navigation;
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Graph g = new Graph();
+           
+            Node n =  new Node(0);
+            Node n2 = new Node(1);
+            Node n3 = new Node(7);
+            Node n4 = new Node(6);
+            Node n5 = new Node(8);
+            Node n6 = new Node(12);
+            Node n7 = new Node(22);
+
+            g.nodes.Add(n);
+            g.nodes.Add(n2);
+            g.nodes.Add(n3);
+            g.nodes.Add(n4);
+            g.nodes.Add(n5);
+            g.nodes.Add(n6);
+            g.nodes.Add(n7);
+
+
+            Console.WriteLine("list size: "+g.nodes.Count);
+            /*
+             try
+             {
+                 Console.WriteLine(g.nodes.Find((Node obj) => { return obj.getId() == 1; }).getId());
+             }
+             catch (Exception ex)
+             {
+                 Console.WriteLine(ex.Message);
+
+             }
+
+             Console.WriteLine("list size: " + g.nodes.Count);
+ */
+            n.addLink(n3,8);
+            n.addLink(n4,2);
+            n4.addLink(n2, 80);
+            n3.addLink(n,666);
+            n3.addLink(n4, 9);
+            n6.addLink(n3,76);
+            n7.addLink(n,24);
+            n7.addLink(n5, 21);
+
+            g.computeWeights();
+            g.printL();
+
+            n7.removeLink(0);
+
+			g.computeWeights();
+			g.printL();
+
+        }
+    }
+}
